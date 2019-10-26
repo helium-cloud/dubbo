cc4dadf32755caa359e06fbfa5249f1f22acaa8d
wuhao <wuhaotx@feinno.com>
2019-10-26 13:42:16 +0800

    aic bu修复

--- dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/AbstractInterfaceConfig.java
+++ dubbo-config/dubbo-config-api/src/main/java/org/apache/dubbo/config/AbstractInterfaceConfig.java
@@ -624,8 +624,8 @@ public abstract class AbstractInterfaceConfig extends AbstractMethodConfig {
                     Map<String, String> configParams = cc.getParameters() == null ? new HashMap<>() : cc.getParameters();
                     configParams.putAll(rc.getParameters());
                     cc.setParameters(configParams);
+                    cc.getParameters().put(org.apache.dubbo.remoting.Constants.CLIENT_KEY,rc.getClient());
                 }
-                cc.getParameters().put(org.apache.dubbo.remoting.Constants.CLIENT_KEY,rc.getClient());
                 cc.setProtocol(rc.getProtocol());
                 cc.setAddress(rc.getAddress());
                 cc.setUsername(rc.getUsername());
